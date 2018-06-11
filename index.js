281cf0f1efd65d76415aeb6e4d215afa871cfe51
Francesco Novy <francesco@cropster.com>
2018-06-11 13:14:39 +0200

    Initial implementation

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-deploy-latest-revision'
+  name: 'ember-cli-deploy-latest-revision',
+
+  createDeployPlugin: function(options) {
+    return {
+      name: options.name,
+
+      willActivate: function(context) {
+        if (context.commandOptions.revision !== 'LATEST') {
+          return;
+        }
+
+        let allRevisions = context.initialRevisions;
+        let latestRevision = allRevisions[0];
+
+        if (latestRevision) {
+          context.commandOptions.revision = latestRevision.revision;
+        }
+      }
+    };
+  }
 };
